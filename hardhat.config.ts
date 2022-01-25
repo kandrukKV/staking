1d2dbe270be35494e7c12ab9fc6cbdcedba37508
Kandruk Konstantin <kandruk@ya.ru>
2022-01-25 22:38:42 +0700

    first commit

---
+++ hardhat.config.ts
@@ -0,0 +1,38 @@
+import * as dotenv from "dotenv";
+
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-waffle";
+import "@typechain/hardhat";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: process.env.ALCHEMY_URL || "",
+      accounts:
+        process.env.PRIVATE_ACC_KEY !== undefined &&
+        process.env.PRIVATE_ACC_KEY1 !== undefined &&
+        process.env.PRIVATE_ACC_KEY2 !== undefined
+          ? [
+              process.env.PRIVATE_ACC_KEY,
+              process.env.PRIVATE_ACC_KEY1,
+              process.env.PRIVATE_ACC_KEY2,
+            ]
+          : [],
+    },
+    hardhat: {
+      chainId: 31337,
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+};
+
+export default config;
